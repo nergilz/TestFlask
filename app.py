4bce9bdeb86f4ec2f60d9918e7bb4e4eeeea570e
IgorChicherin <ashzak990@gmail.com>
2018-02-11 20:09:40 +0300

    celery task

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
-
+from celery import Celery
 from config import Configuration
-
 
 app = Flask(__name__)
 
 app.config.from_object(Configuration)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:32768'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:32768'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
--- view.py
+++ view.py
@@ -1,15 +1,32 @@
-from app import app
+from app import app, celery
 from flask import render_template
 from realty_parser_req.terrarium.avito_spider import AvitoSpider
 from realty_parser_req.terrarium.cian_spider import CianSpider
 from realty_parser_req.realty_peeewee import ItemsDB
 
+
 @app.route('/')
 def index():
-    # avito_parser = AvitoSpider()
-    # avito_parser.parse()
-    # cian_spider = CianSpider()
-    # cian_spider.parse()
-    database = ItemsDB()
-    item = database.get(id=2)
-    return render_template(template_name_or_list='index.html', item=item)
+    # start_parsing.delay('cian')
+    task = hello.delay()
+    result = task.traceback
+    # database = ItemsDB()
+    # item = database.get(id=2)
+    return render_template(template_name_or_list='index.html', item=result)
+
+
+@celery.task
+def start_parsing(site):
+    if site == 'avito':
+        avito_parser = AvitoSpider()
+        avito_parser.parse()
+    elif site == 'cian':
+        cian_spider = CianSpider()
+        cian_spider.parse()
+    return True
+
+
+@celery.task
+def hello():
+    text = 'task is run!!!'
+    return 'Hello %s' % (text)
