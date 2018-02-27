dce550a0f70491c769b4872d8196191f9f443c89
IgorChicherin <ashzak990@gmail.com>
2018-02-27 11:14:14 +0300

    admin app register

---
+++ admin/blueprint.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask import render_template
+
+
+admin = Blueprint('admin', __name__, template_folder='templates')
+
+
+@admin.route('/')
+def index():
+    return render_template('admin/index.html')
