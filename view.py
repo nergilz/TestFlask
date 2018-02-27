d2260a98192cbfd94a2b7feedb9bea631260dc09
IgorChicherin <ashzak990@gmail.com>
2018-02-27 12:34:54 +0300

    veiw results from db in main page

--- view.py
+++ view.py
@@ -1,13 +1,10 @@
 from app import app
 from flask import render_template
 
-from tasks import hello, start_parsing
-
 from realty_parser.models import ItemsDB
-from realty_parser.spiders.cian_spider import CianSpider
-
 
 @app.route('/')
 def index():
-    task = start_parsing.delay('yandex')
-    return render_template(template_name_or_list='index.html', task_id=task.id, status=task.status)
+    database = ItemsDB()
+    records = database.select().order_by(ItemsDB.id)
+    return render_template(template_name_or_list='index.html', records=records)
