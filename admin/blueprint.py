c42ec6bf7a8a93ba82e66b55febfe3080423e826
IgorChicherin <ashzak990@gmail.com>
2018-02-27 16:01:11 +0300

    added run-task route

--- admin/blueprint.py
+++ admin/blueprint.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 from flask import render_template
 
-
 admin = Blueprint('admin', __name__, template_folder='templates')
 
 
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
+
+
+@admin.route('/run-task/')
+def run_task(site):
+    pass
