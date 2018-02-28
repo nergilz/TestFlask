08c950964c51ec9f763918f5a9b320491c1c9829
IgorChicherin <ashzak990@gmail.com>
2018-02-28 17:52:39 +0300

    added excepton

--- tasks.py
+++ tasks.py
@@ -9,15 +9,20 @@ from realty_parser.spiders.yandex_spider import YandexSpider
 
 @celery.task()
 def start_parsing(site):
-    if site == 'avito':
-        avito_parser = AvitoSpider()
-        avito_parser.parse()
-    elif site == 'cian':
-        cian_spider = CianSpider()
-        cian_spider.parse()
-    elif site == 'yandex':
-        yandex_spider = YandexSpider()
-        yandex_spider.parse()
+    try:
+        if site == 'avito':
+            avito_parser = AvitoSpider()
+            avito_parser.parse()
+        elif site == 'cian':
+            cian_spider = CianSpider()
+            cian_spider.parse()
+        elif site == 'yandex':
+            yandex_spider = YandexSpider()
+            yandex_spider.parse()
+    except Exception:
+        return False
+    finally:
+        return True
 
 
 @celery.task()
