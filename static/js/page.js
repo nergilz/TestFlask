15ac7ffd24347c108201707b9f1e0e5c3760292b
IgorChicherin <ashzak990@gmail.com>
2018-02-28 14:06:01 +0300

    working add progressbar on page

--- static/js/page.js
+++ static/js/page.js
@@ -11,8 +11,18 @@ function getOption() {
 }
 
 function sendStartTaskData() {
+    var value = getOption().value;
     lockOption();
-
+    var divToInsert = document.getElementById('#progress-bar-div');
+    divToInsert.insertAdjacentHTML(
+        'beforeend',
+        `<div class="progress" id="progress-bar-${value}">
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0"
+                 aria-valuemax="100" style="width: 100%">
+                <span>${value.charAt(0).toUpperCase() + value.slice(1)} is parsing. Please wait<span class="dotdotdot"></span></span>
+            </div>
+        </div>`
+    );
 }
 
 function checkStatus () {
