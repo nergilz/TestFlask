bcffebec7a8b8d661bc9547c4b2027b3e351f7a3
IgorChicherin <ashzak990@gmail.com>
2018-03-02 17:01:51 +0300

    docker ready app

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Configuration(object):
     DEBUG = True
-    CELERY_BROKER_URL = 'redis://localhost:32768/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:32768/0'
+    CELERY_BROKER_URL = 'redis://redis:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from realty_parser.models import ItemsDB
 if __name__ == '__main__':
     db = ItemsDB()
     db.create_db()
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
 
